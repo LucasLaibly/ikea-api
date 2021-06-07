ba74726fb59b9e43896438533d61f258beab073a
Lucas Laibly <original7245@gmail.com>
2021-06-06 21:21:56 -0700

    get all items in a customers cart func

--- api/models/Cart.go
+++ api/models/Cart.go
@@ -41,3 +41,29 @@ func (cart *Cart) SaveCart(db *gorm.DB) (*Cart, error) {
 
 	return cart, nil
 }
+
+/*
+Get all items in a customer's cart
+*/
+func (c *Cart) GetAllItemsInCart(db *gorm.DB, uuid string) (*[]Cart, error) {
+	var err error
+
+	cart := []Cart{}
+
+	err = db.Debug().Model(&Cart{}).Limit(100).Find(&c).Error
+
+	if err != nil {
+		return &[]Cart{}, err
+	}
+
+	if len(cart) > 0 {
+		for i := range cart {
+			err := db.Debug().Model(&Cart{}).Where("id = ?", cart[i].CustomerID).Take(&cart[i].Customer).Error
+			if err != nil {
+				return &[]Cart{}, err
+			}
+		}
+	}
+
+	return &cart, nil
+}
